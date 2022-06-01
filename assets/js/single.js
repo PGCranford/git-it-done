c6549156da9c964c1173b413cad4c5895caea5cf
PGCranford <PGCranford@gmail.com>
2022-06-01 13:35:19 -0400

    warning message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,8 +8,11 @@ var getRepoIssues = function (repo) {
         //request was successful
         if (response.ok) {
             response.json().then(function (data) {
-
                 displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             });
 
         }
@@ -52,6 +56,15 @@ var displayIssues = function (issues) {
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
+};
+
+var displayWarning = function (repo) {
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
 };
 
 getRepoIssues("facebook/react");
